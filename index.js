9bb43d8b63da215200a7557e04ac4747b2625b75
Greg Cochard <greg.cochard@gmail.com>
2017-04-04 15:22:23 -0700

    Fix openshift interface problem

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const port = process.env.PORT || 8080;
+const iface = process.env.OPENSHIFT_NODEJS_IP;
 const server = require('./modules/server');
-server.listen(process.env.PORT || 8080, function () {
+server.listen(port, iface, function () {
   console.log('%s listening at %s', server.name, server.url); //eslint-disable-line
 });
